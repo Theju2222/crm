70e483f756b0753d6cfc63e24e008114d284ee5a
Thejas B M <108926913+Theju2222@users.noreply.github.com>
2023-09-22 13:33:45 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-$host = "crm_izb_";
-$username = "crm_izb";
-$password = "Techrowth123";
-$database = "crm_izb__";
+$host = "izb-crm-server.mysql.database.azure.com";
+$username = "xjqxvhraxl";
+$password = "Y4P8GM87D77DN66U$";
+$database = "izb-crm-database";
